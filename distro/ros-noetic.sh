21145ad6bf8d478f14903e8e550d7cc34437ca34
maehara-keisuke <qqu72sx9n@gmail.com>
2021-12-28 12:27:59 +0900

    add installation script for ROS Noetic(ROS Base)

---
+++ distro/ros-noetic.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+R="$(printf '\033[1;31m')"
+G="$(printf '\033[1;32m')"
+Y="$(printf '\033[1;33m')"
+W="$(printf '\033[1;37m')"
+C="$(printf '\033[1;36m')"
+
+banner() {
+    clear
+    printf "\033[33m    _  _ ___  _  _ _  _ ___ _  _    _  _ ____ ___  \033[0m\n"
+    printf "\033[36m    |  | |__] |  | |\ |  |  |  |    |\/| |  | |  \ \033[0m\n"
+    printf "\033[32m    |__| |__] |__| | \|  |  |__|    |  | |__| |__/ \033[0m\n"
+    printf "\033[0m\n"
+    printf "     \033[32mA modded gui version of ubuntu for Termux\033[0m\n"
+    printf "\033[0m\n"
+
+}
+
+ros-noetic() {
+    echo -e "${R} [${W}-${R}]${C} Installing ROS Noetic(ROS Base) ..."${W}
+
+    sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+    sudo apt install curl # if you haven't already installed curl
+    curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+    sudo apt update
+    sudo apt install ros-noetic-desktop
+}
+
+banner
+ros-noetic
